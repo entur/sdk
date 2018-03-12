e7ce60d8fff0a7bdbc58091e5a1e84dffb785c6d
Kent Andersen <kent.andersen@bekk.no>
2018-03-12 14:54:35 +0100

    added get-departures example

---
+++ example/get-departures.js
@@ -0,0 +1,28 @@
+const { default: EnturService } = require('../lib');
+
+const service = new EnturService({
+    environment: 'PROD',
+});
+const now = new Date();
+
+function toTimeString(date) {
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    return `${hour}:${minute}`;
+}
+
+function minutesDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    return Math.floor(timeDiff / (1000 * 60));
+}
+
+service.getStopPlaceDepartures('NSR:StopPlace:14202').then(departures => departures.forEach((departure) => {
+    const { expectedDepartureTime, destinationDisplay, serviceJourney } = departure;
+    const departureTime = new Date(expectedDepartureTime);
+
+    const minDiff = minutesDifference(now, departureTime);
+    const departureLabel = minDiff < 15 ? `${minDiff} min` : toTimeString(departureTime);
+
+    const { line } = serviceJourney.journeyPattern;
+    console.log(`${departureLabel} ${line.transportMode} ${line.publicCode} ${destinationDisplay.frontText}`); // eslint-disable-line no-console
+}));
