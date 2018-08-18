2c68f18f8d85924ec528841fc49a96bc36bd12d2
Kent Andersen <kent.andersen@bekk.no>
2018-08-18 12:51:46 +0200

    moved pipe to start of line in mode union types

--- flow-types/Mode.js
+++ flow-types/Mode.js
@@ -1,37 +1,36 @@
 // @flow
 export type TransportMode =
-    'air' |
-    'bus' |
-    // 'cableway' |
-    'water' |
-    // 'funicular' |
-    // 'lift' |
-    'rail' |
-    'metro' |
-    'tram' // |
-    // 'coach' |
-    // 'unknown'
+    | 'air'
+    | 'bus'
+    // | 'cableway'
+    | 'water'
+    // | 'funicular'
+    // | 'lift'
+    | 'rail'
+    | 'metro'
+    | 'tram'
+    | 'coach'
+    // | 'unknown'
 
 
 export type LegMode =
-    TransportMode |
-    'bicycle' |
-    'coach' |
-    'foot' // |
-    // 'car' |
-    // 'transit' |
-    // 'car_park' |
-    // 'car_pickup' |
+    | TransportMode
+    | 'bicycle'
+    | 'foot'
+    // | 'car'
+    // | 'transit'
+    // | 'car_park'
+    // | 'car_pickup'
 
 
 export type TransportSubmode =
-    'airportLinkRail' |
-    'highSpeedPassengerService' |
-    'highSpeedVehicleService' |
-    'internationalCarFerry' |
-    'localCarFerry' |
-    'localPassengerFerry' |
-    'nationalCarFerry' |
-    'railReplacementBus' |
-    'regionalCarFerry' |
-    'touristRailway'
+    | 'airportLinkRail'
+    | 'highSpeedPassengerService'
+    | 'highSpeedVehicleService'
+    | 'internationalCarFerry'
+    | 'localCarFerry'
+    | 'localPassengerFerry'
+    | 'nationalCarFerry'
+    | 'railReplacementBus'
+    | 'regionalCarFerry'
+    | 'touristRailway'
