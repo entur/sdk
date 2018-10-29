a8e424ffc63451d841c3de2b58957af76734746c
Mats Byrkjeland <matsbyr@gmail.com>
2018-10-29 14:10:36 +0100

    Adjust rate limit values

--- src/constants/rateLimits.js
+++ src/constants/rateLimits.js
@@ -1,3 +1,3 @@
-export const MAX_CALLS_PER_SECOND = 50
-export const MAX_CALLS_PER_MINUTE = 2000
-export const MAX_CALLS_PER_HOUR = 80000
+export const MAX_CALLS_PER_SECOND = 25
+export const MAX_CALLS_PER_MINUTE = 1000
+export const MAX_CALLS_PER_HOUR = 40000
