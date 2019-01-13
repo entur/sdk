4d470ef0b58df8bb9afbb2b03d4dba0e7684e697
Kent Andersen <kent.andersen@bekk.no>
2019-01-13 21:05:17 +0100

    added typing to getFeatures params

--- src/geocoder/index.js
+++ src/geocoder/index.js
@@ -6,33 +6,51 @@ import type { Feature } from '../../flow-types/Feature'
 import type { Coordinates } from '../../flow-types/Coordinates'
 
 type PositionParam = {
-    'focus.point.lat'?: number,
-    'focus.point.lon'?: number
+    'focus.point.lat': number,
+    'focus.point.lon': number
 }
 
-function getPositionParamsFromGeolocationResult(coords?: Coordinates): PositionParam {
+function getPositionParamsFromGeolocationResult(coords?: Coordinates): PositionParam | void {
     if (!coords) {
-        return {}
+        return
     }
 
     const { latitude, longitude } = coords
+    // eslint-disable-next-line consistent-return
     return {
         'focus.point.lat': latitude,
         'focus.point.lon': longitude,
     }
 }
 
+type GetFeaturesParam = {
+    'boundary.rect.min_lon'?: number,
+    'boundary.rect.max_lon'?: number,
+    'boundary.rect.min_lat'?: number,
+    'boundary.rect.max_lat'?: number,
+    'boundary.country'?: string,
+    sources?: Array<string>,
+    layers?: Array<string>,
+    limit?: number,
+}
 export function getFeatures(
     text: string,
     coords?: Coordinates,
-    params?: Object = {},
+    params?: GetFeaturesParam = {},
 ): Promise<Array<Feature>> {
     const { host, headers } = getGeocoderHost(this.config)
+    const {
+        sources, layers, limit, ...rest
+    } = params
+
     const searchParams = {
-        ...getPositionParamsFromGeolocationResult(coords),
-        lang: 'no',
         text,
-        ...params,
+        lang: 'no',
+        ...getPositionParamsFromGeolocationResult(coords),
+        sources: sources ? sources.join(',') : undefined,
+        layers: layers ? layers.join(',') : undefined,
+        size: limit,
+        ...rest,
     }
 
     const url = `${host}/autocomplete`
