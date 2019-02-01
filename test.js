beb10fd4466d121ebdb897ba6a836edfcbd94c23
Kent Andersen <kent.andersen@bekk.no>
2019-02-01 10:34:12 +0100

    Updated cli tests

--- test.js
+++ test.js
@@ -9,6 +9,13 @@ require('@babel/register')({
     ignore: [/node_modules/],
 })
 
+const JERNBANETORGET_BIKE_STOP = '272'
+
+const LILLEHAMMER_STASJON = 'NSR:StopPlace:420'
+const HAMAR_STASJON = 'NSR:StopPlace:219'
+const JERNBANETORGET = 'NSR:StopPlace:58366'
+const OSLO_S = 'NSR:StopPlace:59872'
+
 const { default: EnturService } = require('./src')
 
 const service = new EnturService({
@@ -18,15 +25,34 @@ const service = new EnturService({
 function getMetodToRun(name) {
     switch (name) {
         case 'getStopPlace':
-            return service.getStopPlace('NSR:StopPlace:58366')
+            return service.getStopPlace(JERNBANETORGET)
+        case 'getStopPlaces':
+            return service.getStopPlaces([
+                OSLO_S,
+                JERNBANETORGET,
+                HAMAR_STASJON,
+                LILLEHAMMER_STASJON,
+            ])
         case 'getStopPlaceFacilities':
-            return service.getStopPlaceFacilities('NSR:StopPlace:58366')
-        case 'getQuaysForStopPlace':
-            return service.getQuaysForStopPlace('NSR:StopPlace:58366')
+            return service.getStopPlaceFacilities(JERNBANETORGET)
+        case 'getQuaysFromStopPlace':
+            return service.getQuaysFromStopPlace(JERNBANETORGET)
         case 'getBikeRentalStation':
-            return service.getBikeRentalStation('470')
-        case 'getDeparturesForStopPlace':
-            return service.getDeparturesForStopPlace('NSR:StopPlace:58366')
+            return service.getBikeRentalStation(JERNBANETORGET_BIKE_STOP)
+        case 'getBikeRentalStationsByPosition':
+            return service.getBikeRentalStationsByPosition({
+                latitude: 59.911898,
+                longitude: 10.75038,
+            }, 50)
+        case 'getDeparturesFromStopPlace':
+            return service.getDeparturesFromStopPlace(JERNBANETORGET)
+        case 'getDeparturesFromStopPlaces':
+            return service.getDeparturesFromStopPlaces(
+                [JERNBANETORGET, HAMAR_STASJON, LILLEHAMMER_STASJON],
+                { limit: 2 },
+            )
+        case 'getDeparturesBetweenStopPlaces':
+            return service.getDeparturesBetweenStopPlaces(LILLEHAMMER_STASJON, HAMAR_STASJON)
         default:
             // eslint-disable-next-line prefer-promise-reject-errors
             return Promise.reject(`Error: method ${name} is not defined`)
