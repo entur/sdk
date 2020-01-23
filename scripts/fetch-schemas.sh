7339718aacf0b894ba8ac551488564607cddb4c1
Mats Byrkjeland <mats.byrkjeland@entur.org>
2020-01-23 12:34:07 +0100

    Don't prettify schema json
    There's no need to have this dependency on a json prettifier like python json.tool

--- scripts/fetch-schemas.sh
+++ scripts/fetch-schemas.sh
@@ -5,11 +5,11 @@ curl 'https://api.entur.io/journey-planner/v2/graphql' --compressed \
     -H 'Pragma: no-cache' \
     -H 'Cache-Control: no-cache' \
     --data '{"query":" query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } }\n fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } }\n fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue }\n fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }\n ","operationName":"IntrospectionQuery"}' \
-    | python -m json.tool > schemas/journeyplanner2.json
+    > schemas/journeyplanner2.json
 
 curl 'https://api.entur.io/stop-places/v1/graphql' --compressed \
     -H 'Content-Type: application/json' \
     -H 'Pragma: no-cache' \
     -H 'Cache-Control: no-cache' \
     --data '{"query":" query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } }\n fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } }\n fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue }\n fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }\n ","operationName":"IntrospectionQuery"}' \
-    | python -m json.tool > schemas/nsr.json
+    > schemas/nsr.json
