8359db7666ed4639ebbf435c9a1a94ae587fc84d
Torbjørn Fjørtoft <torfjor@fjortoft.org>
2020-02-04 14:12:37 +0100

    use agent with keepalive

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,3 +1,12 @@
-import nodeFetch from 'node-fetch'
+import nodeFetch, { RequestInit, RequestInfo } from "node-fetch";
+import { HttpsAgent as Agent } from "agentkeepalive";
 
-export default nodeFetch
+const agent = new Agent({
+    keepAlive: true
+});
+
+export default (url: RequestInfo, init?: RequestInit) =>
+    nodeFetch(url, {
+        agent,
+        ...init
+    });
