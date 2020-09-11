e4c4afb7357f8564d8d785d7f8d8328a0fc9e696
Mats Byrkjeland <mats.byrkjeland@entur.org>
2020-09-11 10:53:54 +0200

    Update dependencies, remove now unnecessary custom types for promise-throttle

--- typings/promise-throttle/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'promise-throttle' {
-    export default class PromiseThrottle {
-        constructor(options?: {
-            requestsPerSecond?: number;
-        })
-
-        add<T>(func: () => Promise<T>): Promise<T>
-    }
-}
