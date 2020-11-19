d80813f337282d1a1f4c5878cf30dc86d53fcd71
Mats Byrkjeland <mats.byrkjeland@entur.org>
2020-11-19 16:31:08 +0100

    Remove deprecated FeatureCategory constants, use enum instead

--- src/constants/featureCategory.ts
+++ src/constants/featureCategory.ts
@@ -1,25 +1,3 @@
-// @deprecated: Use the FeatureCategory enum instead of individual exports
-export const ONSTREET_BUS = 'onstreetBus'
-export const ONSTREET_TRAM = 'onstreetTram'
-export const AIRPORT = 'airport'
-export const RAIL_STATION = 'railStation'
-export const METRO_STATION = 'metroStation'
-export const BUS_STATION = 'busStation'
-export const COACH_STATION = 'coachStation'
-export const TRAM_STATION = 'tramStation'
-export const HARBOUR_PORT = 'harbourPort'
-export const FERRY_PORT = 'ferryPort'
-export const FERRY_STOP = 'ferryStop'
-export const LIFT_STATION = 'liftStation'
-export const VEHICLE_RAIL_INTERCHANGE = 'vehicleRailInterchange'
-export const GROUP_OF_STOP_PLACES = 'GroupOfStopPlaces'
-export const POI = 'poi'
-export const VEGADRESSE = 'Vegadresse'
-export const STREET = 'street'
-export const TETTSTEDDEL = 'tettsteddel'
-export const BYDEL = 'bydel'
-export const OTHER = 'other'
-
 export enum FeatureCategory {
     ONSTREET_BUS = 'onstreetBus',
     ONSTREET_TRAM = 'onstreetTram',
