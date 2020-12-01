8037e7e04e773cdf54d30f0f0c363a05ec0b3c9e
Mats Byrkjeland <mats.byrkjeland@entur.org>
2020-12-01 16:38:21 +0100

    Export types through index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,43 @@ export {
 } from './utils'
 
 export { TypeName } from './nearest/types'
+export type { NearestPlace } from './nearest/types'
 export { getTripPatternsQuery } from './trip'
 export { isBatteryScooter, isBatteryLevelScooter } from './scooters'
 export { ScooterOperator, BatteryLevel } from './scooters/types'
+export type { Scooter } from './scooters/types'
 
-export * from './types/Mode'
 export * from './constants/featureCategory'
 
 export { journeyPlannerQuery, nsrQuery } from './api'
 
+export type { DeparturesById } from './departure'
 export { County } from './geocoder/countyIds'
 
 export default EnturService
+
+export type { Authority } from './fields/Authority'
+export type { BikeRentalStation } from './fields/BikeRentalStation'
+export type { BookingArrangement } from './fields/BookingArrangement'
+export type { Departure } from './fields/Departure'
+export type { EstimatedCall } from './fields/EstimatedCall'
+export type { Interchange } from './fields/Interchange'
+export type { Leg } from './fields/Leg'
+export type { Line } from './fields/Line'
+export type { Notice } from './fields/Notice'
+export type { Operator } from './fields/Operator'
+export type { Place } from './fields/Place'
+export type { PointsOnLink } from './fields/PointsOnLink'
+export type { Quay } from './fields/Quay'
+export type { ServiceJourney } from './fields/ServiceJourney'
+export type { Situation } from './fields/Situation'
+export type { StopPlace } from './fields/StopPlace'
+
+export type { Coordinates } from './types/Coordinates'
+export type { DestinationDisplay } from './types/DestinationDisplay'
+export type { Feature } from './types/Feature'
+export type { FlexibleLineType } from './types/FlexibleLineType'
+export * from './types/Mode'
+export type { Location } from './types/Location'
+export type { MultilingualString } from './types/MultilingualString'
+export type { StopPlaceDetails, StopPlaceFacilities } from './types/StopPlace'
