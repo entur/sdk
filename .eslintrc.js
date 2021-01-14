bf55aa5beca6977f5a06dbb5e5c13da71be30c63
Mats Byrkjeland <mats.byrkjeland@entur.org>
2021-01-14 11:33:58 +0100

    Format .eslintrc.js, .prettierrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,11 @@ module.exports = {
     env: {
         node: true,
     },
-    plugins: [
-        '@typescript-eslint',
-        'prettier',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     rules: {
         '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
         '@typescript-eslint/member-delimiter-style': 'off',
         'prettier/prettier': 'error',
-    }
+    },
 }
