06c6493c0f79ae32ac04d00e0d83863ff5b45b6f
Mats Byrkjeland <mats.byrkjeland@entur.org>
2021-04-22 17:24:17 +0200

    Add TS doc for GetVehiclesParams

--- src/mobility/getVehicles/index.ts
+++ src/mobility/getVehicles/index.ts
@@ -7,15 +7,25 @@ import { FormFactor, PropulsionType, Vehicle } from '../types'
 import getVehiclesQuery from './query'
 
 export interface GetVehiclesParams {
+    /** The latitude coordinate. */
     lat: number
+    /** The longitude coordinate. */
     lon: number
+    /** The radius in meters from the coordinate pair in which to find vehicles. */
     range: number
+    /** The maximum number of vehicles to return. */
     count?: number
+    /** Return only vehicles of the given operators.  */
     operators?: string[]
+    /** The maximum number of vehicles to return */
     codespaces?: string[]
+    /** Return only vehicles of the given form factors. */
     formFactors?: FormFactor[]
+    /** Return only vehicles of the given propulsion types. */
     propulsionTypes?: PropulsionType[]
+    /** Whether you want to return vehicles that are already reserved. The default is false. */
     includeReserved?: boolean
+    /** Whether you want to return vehicles that are disabled. The default is false. */
     includeDisabled?: boolean
 }
 
