a4cd8c483fdf1e5f0f2b595f84510b7338d45351
Mats Byrkjeland <mats.byrkjeland@entur.org>
2021-06-18 16:29:40 +0200

    Don't generate API client class

--- scripts/generate-types.sh
+++ scripts/generate-types.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-swagger-typescript-api -p "https://api.entur.io/stop-places/v1/read/api-docs" -n ./src/nsr/types.ts
+swagger-typescript-api -p "https://api.entur.io/stop-places/v1/read/api-docs" -n ./src/nsr/types.ts --no-client
